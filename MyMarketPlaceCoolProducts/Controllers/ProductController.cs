ac85543db9b4c558dbd23d8522fb3cb25853e8e2
Daniel <daniel.acaz@becommerce.com.br>
2020-05-30 12:47:25 -0300

    Criado Post Http na Api de Produtos

--- MyMarketPlaceCoolProducts/Controllers/ProductController.cs
+++ MyMarketPlaceCoolProducts/Controllers/ProductController.cs
@@ -1,10 +1,61 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using MyMarketPlaceCoolProducts.Model;
+
 namespace MyMarketPlaceCoolProducts.Controllers
 {
-    public class ProductController
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductController : ControllerBase
     {
+
+        private static IList<Product> Products = new List<Product>(new Product[]
+            {
+                new Product("Produto1", "DescricaoDoProduto1", "ImagemDoProduto1", 100.0),
+                new Product("Produto2", "DescricaoDoProduto2", "ImagemDoProduto2", 100.0),
+                new Product("produto2", "DescricaoDoProduto3", "ImagemDoProduto3", 100.0)
+            });
+
         public ProductController()
         {
+        }
+
+        [HttpGet]
+        public IList<Product> Get()
+        {
+            return Products;
+        }
+
+        [HttpDelete]
+        [Route("{_Id}")]
+        public Product DeleteById(long _Id)
+        {
+            Product productWillDeleted = Products
+                            .Where(product => product.GetId() == _Id)
+                            .FirstOrDefault();
+            if(Products.Remove(productWillDeleted))
+            {
+                return productWillDeleted;
+            }
+            throw new Exception("Cannot Delete This Product");
+            
+        }
+
+        [HttpPost]
+        public Product CreateProduct([FromBody] Product _product)
+        {
+            Product newProduct = new Product(
+                _product.Title,
+                _product.Description,
+                _product.ImageUrl,
+                _product.Price
+            );
+
+            Products.Add(newProduct);
+
+            return newProduct;
         }
     }
 }
--- MyMarketPlaceCoolProducts/Model/Product.cs
+++ MyMarketPlaceCoolProducts/Model/Product.cs
@@ -3,8 +3,32 @@ namespace MyMarketPlaceCoolProducts.Model
 {
     public class Product
     {
+
+        private static long _Id = 0;
+
         public Product()
         {
         }
+
+        public Product(string title, string description, string imageUrl, double price)
+        {
+            Id = ++_Id;
+            Title = title;
+            Description = description;
+            ImageUrl = imageUrl;
+            Price = price;
+        }
+
+        public long Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+        public double Price { get; set; }
+
+        public long GetId()
+        {
+            return Id;
+        }
+
     }
 }
